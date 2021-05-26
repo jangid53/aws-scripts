a0c90136ed00abd6f54cd281c251fe46427a660b
jangid53 <73091066+jangid53@users.noreply.github.com>
2021-05-27 01:15:02 +0800

    added the logic
    added the logic

--- ec2-self-termination.sh
+++ ec2-self-termination.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+fileName=/opt/rh-sso/deployment.war
+interval=259200 # 3days in seconds
+echo "Starting.."
+lastUpdatedEpochTime=`stat -c "%Y" $fileName`
+currentEpochTime=`date "+%s"`
+difference=`expr $currentEpochTime - $lastUpdatedEpochTime`
+echo "Difference is $difference"
+if [ $difference -gt $interval ]
+then
+        echo "instance is terminating"
+        sudo shutdonw -h now
+else
+        echo "nothing happend"
+fi
